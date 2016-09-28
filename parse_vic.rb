19f4517f36ff1d942480a82d71b815981236f77e
milassa <mila.abramova@gmail.com>
2016-09-29 00:22:24 +0300

    Update parse_vic.rb
    code formatting

--- parse_vic.rb
+++ parse_vic.rb
@@ -11,11 +11,11 @@ doc.css('.product-details').each do |product_details|
 	stick_name = product_details.at_css('h3 a').text
 	product_details.css('.product-attributes').each do |product_attributes|
 		stick_diametr, stick_length = product_attributes.css('small')
-														.map { |data| data.text.gsub(/\[?\]?/, '').to_f }
+								.map { |data| data.text.gsub(/\[?\]?/, '').to_f }
 		sticks.push(
-	    	title: stick_name,
-	    	diametr: stick_diametr,
-	    	length: stick_length
+	    		title: stick_name,
+	    		diametr: stick_diametr,
+	    		length: stick_length
 		)
 	end
 end
